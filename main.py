692f0dac951f9bb68ebb08c8705628cf33e8c856
celine <celine.einspieler@a1.net>
2024-01-09 10:44:37 +0100

    fixes

--- main.py
+++ main.py
@@ -16,10 +16,12 @@ from kivy.uix.filechooser import FileChooserListView
 from kivy.uix.popup import Popup
 from kivy.graphics import Line
 from kivymd.uix.progressbar import MDProgressBar
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
 
 Builder.load_file('kivyy.kv')
 
-#bild links oben 
+#bild links oben
 Config.set('kivy', 'window_icon', 'image/bild.png')
 
 
@@ -87,7 +89,7 @@ class WelcomeView(GridLayout):
         super().__init__(**kwargs)
         self.screen_manager = screen_manager
         
-        # ausrichtung vom welcome screen -> hier mittig
+        # ausrichtung vom welcome screen -> mittig
         self.cols = 1
         self.size_hint = (0.6, 0.7)
         self.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
@@ -100,60 +102,24 @@ class WelcomeView(GridLayout):
 
         # ladebalken einfügen
         self.progress_bar = MyProgressBar(welcome_view=self, max=100)
-        #self.progress_bar = CircularProgressBar(welcome_view=self, max=80) #kreisladebalken
-
         self.add_widget(self.progress_bar)
-
         # laden
         Clock.schedule_interval(self.progress_bar.update_progress, 1 / 25)
-        #Clock.schedule_interval(self.animate_progress, 0.1) #kreisladebalken
      
     def switch_to_mainview(self):
         self.screen_manager.transition.direction = 'left'
         self.screen_manager.current = 'mainview'
-        
 
 
+#für den dialog fenster        
+KV = '''
+MDFloatLayout:
 
-"""
-class FileContentView(BoxLayout):
-    def __init__(self, filechooser, file_path, **kwargs):
-        super().__init__(**kwargs)
-        self.orientation = 'vertical'
-        self.filechooser = filechooser
-
-        # Zurück-Button
-        back_button = Button(text='Zurück', size_hint_y=None, height=50)
-        back_button.bind(on_press=self.go_back)
-        self.add_widget(back_button)
-
-        # Dateiinhalt anzeigen
-        try:
-            with open(file_path, 'r') as file:
-                file_content = file.read()
-
-            content_label = Label(text=f"Inhalt der ausgewählten Datei:\n{file_content}")
-            self.add_widget(content_label)
-
-        except Exception as e:
-            print(f"Fehler beim Lesen der Datei: {e}")
-
-    def go_back(self, instance):
-        # Zurück zum Filechooser
-        self.filechooser.clear_widgets()
-        self.filechooser.add_widget(Filechooser())
-
-    def show_file_content(self, file_path):
-        try:
-            with open(file_path, 'r') as file:
-                file_content = file.read()
-
-            self.content_label.text = f"Inhalt der ausgewählten Datei:\n{file_content}"
-
-        except Exception as e:
-            print(f"Fehler beim Lesen der Datei: {e}")
-"""
-
+    MDFlatButton:
+        text: "ALERT DIALOG"
+        pos_hint: {'center_x': .5, 'center_y': .5}
+        on_release: app.show_alert_dialog()
+'''
 
 class MainView(BoxLayout):
     def __init__(self, **kwargs):
@@ -186,102 +152,21 @@ class MainView(BoxLayout):
         
         else:
             print("Keine Datei ausgewählt.")
+    
     def press_compare(self):
         if self.ids.label_1.text and self.ids.label_2.text:
-            print("comparing now !!!")
+            dialog = MDDialog(
+                text="Berechnungen",
+                buttons=[
+                    MDFlatButton(
+                        text="OK",
+                        on_release=lambda *args: dialog.dismiss()
+                    )
+                ]
+            )
+            dialog.open()
 
 
-
-"""
-###############
-class Filechooser(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.filechooser_1 = None
-        self.filechooser_2 = None
-        self.content_label = Label()  # Assuming content_label is a Kivy Label
-        self.add_widget(self.content_label)  # Add content_label to the layout
-
-    def select(self, file_chooser, instance, value):
-        if value:
-            selected_file = value[0]
-
-            try:
-                with open(selected_file, 'r') as file:
-                    file_content = file.read()
-
-                self.content_label.text = f'Inhalt der ausgewählten Datei:\n{file_content}'
-
-            except Exception as e:
-                print(f"Fehler beim Lesen der Datei {selected_file}: {e}")
-        else:
-            print("Keine Datei ausgewählt.")
-
-
-#########################
-
-
-
-                if file_chooser == self.filechooser_1:
-                    current_label = self.ids.label_1
-                elif file_chooser == self.filechooser_2:
-                    current_label = self.ids.label_2
-                else:
-                    return
-
-                current_label.text = f'Inhalt der ausgewählten Datei:\n{file_content}'
-
-
-#######################
-                
-
-        #todo: soll dann nur gehen wenn zwei files ausgwählt worden sind
-        self.select_files_button = Button(text='Select Files', size_hint_y=None, height=50)
-        self.select_files_button.bind(on_press=self.show_progress_bar)
-        self.add_widget(self.select_files_button)
-
-        self.progress_bar = None
-        
-    def show_progress_bar(self, instance):
-        self.remove_widget(self.select_files_button) #löscht den button und ersetzt ihn mit:
-        #einem kreisladebalken->
-        self.progress_bar = CircularProgressBar(welcome_view=None, max=100)
-        self.add_widget(self.progress_bar)
-        Clock.schedule_interval(self.update_progress, 0.1)
-
-
-    def update_progress(self, dt):
-        if self.progress_bar.value < 100:
-            self.progress_bar.set_value(self.progress_bar.value + 1)
-        else:
-            #todo: nach dem "laden" -> ein popup fenser öffnen für die berechnungen
-            self.show_popup()  # Popup anzeigen
-            Clock.unschedule(self.update_progress)  # Das Aktualisieren des Ladebalkens stoppen
-            pass
-            
-
-    #öffnen dann ein pupup fenster 
-    def show_popup(self):
-        content = BoxLayout(orientation='vertical')
-         # statt dem bild dann die berechnungen
-        image = Image(source='image/bild.png', size=(420, 200))
-        content.add_widget(image)
-
-        close_button = Button(text='Schließen', size_hint_y=None, height=50)
-        close_button.bind(on_press=self.dismiss_popup) #wenn ich auf den button clicke schließt er das pupup fenster
-        content.add_widget(close_button)
-
-
-        popup = Popup(title='Berechnungen', content=content, size_hint=(None, None), size=(700, 300))
-        popup.open()
-
-    def dismiss_popup(self, instance):
-        popup = self.children[0]
-        popup.dismiss()
-
-"""
-##--------------------------------------------------------
-    
 
 
 class Plagiloki(MDApp): #hier den namen links oben ändern
@@ -299,7 +184,7 @@ class Plagiloki(MDApp): #hier den namen links oben ändern
         # filechooser
         mainview_screen = Screen(name='mainview')
         mainview = MainView()
-        mainview_screen.add_widget(mainview)
+        mainview_screen.add_widget(mainview)    
         screen_manager.add_widget(mainview_screen)
         
         return screen_manager
